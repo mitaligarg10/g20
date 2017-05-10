31590ae3fe9110e3d9ee5005e24726b63916cb51
Mitali <mitaligarg94@gmail.com>
2017-05-10 09:32:47 +0530

    g20 submission corrected

--- js/g20Mitali.js
+++ js/g20Mitali.js
@@ -1,5 +1,7 @@
+ /* Importing Modules*/
   const readline = require('readline');
   const fs = require('fs');
+  /*  Variable decleration*/
   let dataOne = [];
   let i = 0;
   let a = [];
@@ -9,6 +11,7 @@
   let countryIndex;
   let populationIndex;
   let gdpIndex;
+  /*  functions to validate start year*/
   module.exports = function convert(startYear)
   {
   if(typeof startYear === 'string') {
@@ -17,9 +20,12 @@
   if(typeof startYear !== 'number' || isNaN(startYear)) {
     throw new Error('Not a number');
   }
+  /*   Creating readstream*/
   const rl = readline.createInterface({
     input: fs.createReadStream('Table1.3_g20_2013.csv')
   });
+/*  functions to read input data line by line and split accordingly with comma(,)
+ to find the index of header*/
   rl.on('line', (line) => {
   if (i === 0)
    {
@@ -29,7 +35,8 @@
       gdpIndex = dataOne.indexOf('GDP Billions (US$) - 2013');
       i = 1;
     }
-
+/* splitting received data by comma and
+push the split data into array*/
   dataOne = line.split(',');
   a.push({country: dataOne[countryIndex], gdp: dataOne[gdpIndex]});
   b.push({country: dataOne[countryIndex], population: dataOne[populationIndex]});
@@ -43,15 +50,15 @@
       b.pop();
       a.shift();
       b.shift();
+      /*  Sorting in descending order*/
        a.sort(function(x, y) {return parseFloat(y.gdp) - parseFloat(x.gdp);});
       b.sort(function(x, y) {return parseFloat(y.population) - parseFloat(x.population);});
-   //    console.log(a);
-      // console.log(b);
+   /* Creating the JSON*/
       aJson = JSON.stringify(a);
       bJson = JSON.stringify(b);
 
-      fs.writeFile('output1.json', aJson);
-      fs.writeFile('output2.json', bJson);
+      fs.writeFile('output1G20Mitali.json', aJson);
+      fs.writeFile('output2G20Mitali.json', bJson);
   }, 500);
   return 'JSON written successfully';
 };
